bc3d9cd62cc3c2593c677177eea8a0de02a73400
Seth Kendall <sethpkendall@gmail.com>
2017-03-05 08:15:27 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+
+ReactDom.render( <Router history={browserHistory} routes={routes}/>, document.querySelector('#app'));
